e8115d4dba8a501cc0b3a37d4572b81a1c11fe2c
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2019-05-20 23:23:28 -0300

    finished

--- src/main/java/io/github/guiritter/sequential_text_file_saver/Main.java
+++ src/main/java/io/github/guiritter/sequential_text_file_saver/Main.java
@@ -1,6 +1,14 @@
 package io.github.guiritter.sequential_text_file_saver;
 
 import java.awt.BorderLayout;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -12,6 +20,8 @@ import javax.swing.JTextArea;
  * @author Guilherme Alan Ritter
  */
 public final class Main {
+
+    private static final String ROOT_PATH = "D:\\social\\eu\\dinheiro\\backup";
 
     static {
         JFrame.setDefaultLookAndFeelDecorated(true);
@@ -25,8 +35,24 @@ public final class Main {
         area.setColumns(20);
         area.setRows(10);
         frame.getContentPane().add(area, BorderLayout.CENTER);
-        
+
         JButton button = new JButton("save");
+        button.addActionListener(actionEvent -> {
+            Path rootPath = Paths.get(ROOT_PATH);
+            short name = (short) (Arrays.stream(rootPath.toFile().listFiles())
+                    .map(file -> Short.parseShort(file.getName().substring(0, 5)))
+                    .reduce((short) 0, (previous, current) -> (short) Math.max(previous, current)) + 1);
+            try {
+                Files.write(rootPath.resolve(String.format("%05d.txt", name)),
+                        Arrays.asList(area.getText().split("\n")), StandardCharsets.UTF_8, StandardOpenOption.CREATE,
+                        StandardOpenOption.TRUNCATE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(frame, "check console", "error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            area.setText("");
+        });
         frame.getContentPane().add(button, BorderLayout.SOUTH);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
